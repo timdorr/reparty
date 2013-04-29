07dd4e5045c94fd23071539e9ba4817085523ed9
timdorr <timdorr@timdorr.com>
2013-04-29 02:39:05 -0400

    Move those tmp files to the root.

--- spec/tasks/reparty_task_spec.rb
+++ spec/tasks/reparty_task_spec.rb
@@ -20,8 +20,12 @@ describe "reparty:email" do
 
   it "should send out via letter_opener", letteropener: true do
     require 'letter_opener'
-    ActionMailer::Base.add_delivery_method :letter_opener, LetterOpener::DeliveryMethod, :location => File.expand_path('../tmp/letter_opener', __FILE__)
+    ActionMailer::Base.add_delivery_method :letter_opener, LetterOpener::DeliveryMethod, :location => File.expand_path('../../../tmp/letter_opener', __FILE__)
     ActionMailer::Base.delivery_method = :letter_opener
+
+    Reparty.config do |config|
+      config.add_report Reparty::Report::ActiveRecord
+    end
 
     subject.invoke("test@test.com")
   end
