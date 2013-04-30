573b4bb356fca6bb2be1866503e4760e2858603c
timdorr <timdorr@timdorr.com>
2013-04-29 22:27:07 -0400

    Add some setup for testing ActiveRecord.
    Taken from groupdate: https://github.com/ankane/groupdate/blob/master/test/groupdate_test.rb

--- reparty.gemspec
+++ reparty.gemspec
@@ -27,5 +27,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rspec"
+  spec.add_development_dependency "sqlite3"
   spec.add_development_dependency "letter_opener"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,25 @@ Dir["./spec/support/**/*.rb"].sort.each {|f| require f}
 
 require 'action_mailer'
 ActionMailer::Base.delivery_method = :test
+
+require 'sqlite3'
+require 'active_record'
+
+ActiveRecord::Base.default_timezone = :utc
+ActiveRecord::Base.time_zone_aware_attributes = true
+
+class User < ActiveRecord::Base
+end
+
+ActiveRecord::Base.establish_connection(
+    :adapter => 'sqlite3',
+    :database => Reparty.root + 'tmp/test.db'
+)
+
+unless ActiveRecord::Base.connection.table_exists? "users"
+  ActiveRecord::Migration.create_table :users do |t|
+    t.string :name
+    t.integer :score
+    t.timestamps
+  end
+end
