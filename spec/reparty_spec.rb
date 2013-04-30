cbda7ba30ffb1078917fe85a24f7a086ece1e33a
timdorr <timdorr@timdorr.com>
2013-04-30 19:22:35 -0400

    Single quotes bug me. :bug:

--- spec/reparty_spec.rb
+++ spec/reparty_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 
 describe Reparty do
-  it 'should have a version number' do
+  it "should have a version number" do
     Reparty::VERSION.should_not be_nil
   end
 
-  it 'configures properly' do
+  it "configures properly" do
     Reparty.config do |config|
-      config.sender = 'test@test.com'
+      config.sender = "test@test.com"
     end
-    Reparty.configuration.sender.should == 'test@test.com'
+    Reparty.configuration.sender.should == "test@test.com"
   end
 
-  it 'adds reports' do
+  it "adds reports" do
     Reparty.config do |config|
       config.add_report Reparty::Report, "Some Report"
     end
