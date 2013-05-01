65441d9a3ad812bc47736939f6b4971a33b7e6a4
timdorr <timdorr@timdorr.com>
2013-05-01 02:30:45 -0400

    Flesh out some of Sendgrid report and grab stats.

--- lib/reparty/report/sendgrid.rb
+++ lib/reparty/report/sendgrid.rb
@@ -1,7 +1,33 @@
+require 'httparty'
+
 module Reparty
   class Report
     class Sendgrid < Report
+      include HTTParty
+      format :json
+      headers 'Content-Type' => 'application/json', 'Accept' => 'application/json'
 
+      attr_reader :api_user, :api_key
+
+      def initialize(*args, &block)
+        super(args.shift)
+
+        @api_user = args.shift
+        @api_key = args.shift
+
+        @color = "#287abe"
+      end
+
+      def stats(today=DateTime.now)
+        @stats ||= self.class.get(
+            "https://sendgrid.com/api/stats.get.json?" +
+            "api_user=" + @api_user +
+            "&api_key=" + @api_key +
+            "&start_date=" + today.utc.strftime("%Y-%m-%d") +
+            "&days=6",
+            options: { format: 'json' }
+        )
+      end
     end
   end
 end
--- spec/report/sendgrid_spec.rb
+++ spec/report/sendgrid_spec.rb
@@ -1,5 +1,26 @@
 require_relative '../spec_helper'
 
-describe Reparty::Report::Sendgrid do
+describe Reparty::Report::Sendgrid, vcr: { cassette_name: "sendgrid" } do
+  let(:sendgrid_user) { ENV.fetch("SENDGRID_USER","admin@someplace.com") }
+  let(:sendgrid_pass) { ENV.fetch("SENDGRID_PASSWORD","asdfasdf") }
 
+  let(:stats) { subject.stats(DateTime.new(2013,5,1)) }
+
+  subject do
+    Reparty.config do |config|
+      config.add_report Reparty::Report::Sendgrid, "Sendgrid Emails", sendgrid_user, sendgrid_pass
+    end
+    Reparty.reports.last
+  end
+
+  it { should be_kind_of(Reparty::Report::Sendgrid) }
+  its(:api_user) { should == sendgrid_user }
+  its(:api_key)  { should == sendgrid_pass }
+  its(:color)    { should == "#287abe" }
+
+  it "gets stats" do
+    stats.size.should == 7
+    stats.first.should be_kind_of(Hash)
+    stats.first["delivered"].should == 3059
+  end
 end
