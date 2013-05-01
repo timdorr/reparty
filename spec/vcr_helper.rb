ca0b7da589560b6bc97a8b6a1f1f6c57dc3e3810
timdorr <timdorr@timdorr.com>
2013-05-01 02:30:19 -0400

    Filter Sendgrid info from VCR.

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -6,4 +6,7 @@ VCR.configure do |c|
   c.hook_into :webmock
   c.default_cassette_options  = { :record => :new_episodes }
   c.configure_rspec_metadata!
+
+  c.filter_sensitive_data("<SENDGRID_USER>")     { ENV.fetch("SENDGRID_USER","admin@someplace.com") }
+  c.filter_sensitive_data("<SENDGRID_PASSWORD>") { ENV.fetch("SENDGRID_PASSWORD","asdfasdf") }
 end
