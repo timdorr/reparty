6196528eda90d4d65ea94f5013687110fc62a84f
timdorr <timdorr@timdorr.com>
2013-05-01 02:52:39 -0400

    build_daily_graph dependency injection.

--- lib/reparty/report.rb
+++ lib/reparty/report.rb
@@ -16,10 +16,10 @@ module Reparty
 
     protected
 
-    def build_daily_graph
+    def build_daily_graph(start_date=DateTime.now.utc)
       g = Gruff::Line.new(545)
       g.title = @title
-      g.labels = Hash[*(1..7).map{|x| [x-1, (DateTime.now - (8-x)).strftime("%-m/%-d")] }.flatten]
+      g.labels = Hash[*(1..7).map{|x| [x-1, (start_date - (8-x)).strftime("%-m/%-d")] }.flatten]
       g
     end
   end
